ae854dfb0170dd8bc716f2a3202d29a56651106b
jonahkrop <67211009+jonahkrop@users.noreply.github.com>
2020-06-20 17:37:24 -0700

    Delete calibrations.R
    don't need it

--- calibrations.R
+++
@@ -1,6 +0,0 @@
-library(lme4)
-
-df = read.csv('/Users/JonahKrop/Documents/Projects/Baseball/test_df.csv')
-
-model = lmer('release_pos_x ~ 1 + (factor(year)|home_team)', data=df[!is.na(df$release_speed),])
-ranef(model)
